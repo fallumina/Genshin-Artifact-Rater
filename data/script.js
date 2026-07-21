93fa75676406a8b1dfc5752da28d4c19db3f1b69
fallumina <fallumina@gmail.com>
2026-07-21 17:20:27 +1000

    inital set up

---
+++ data/script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("calculateButton");
+
+button.addEventListener("click", function () {
+
+    const results = document.getElementById("results");
+
+    results.innerHTML = "The calculator works!";
+
+});
