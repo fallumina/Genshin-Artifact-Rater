44e858f5bcb23febef106110db7a3ec819cbc5ee
fallumina <fallumina@gmail.com>
2026-07-21 17:29:01 +1000

    Update script.js

--- data/script.js
+++ data/script.js
@@ -1,9 +1,27 @@
 const button = document.getElementById("calculateButton");
 
-button.addEventListener("click", function () {
+button.addEventListener("click", () => {
+
+    const set = document.getElementById("artifactSet").value;
 
     const results = document.getElementById("results");
 
-    results.innerHTML = "The calculator works!";
+    results.innerHTML = `
+        <h3>Testing...</h3>
+
+        Artifact Set: ${set}
+
+        <br><br>
+
+        Hu Tao - 95
+
+        <br>
+
+        Diluc - 82
+
+        <br>
+
+        Yoimiya - 76
+    `;
 
 });
