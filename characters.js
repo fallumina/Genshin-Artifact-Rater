e754bfe874f36597eee9745bd5e9715ad1826b3f
fallumina <fallumina@gmail.com>
2026-07-23 19:49:18 +1000

    test substituting neuvi for aino

--- characters.js
+++ characters.js
@@ -1,61 +1,33 @@
 const characters = [
 {
-    character: "Neuvillette",
-    name: "Neuvillette - Hypercarry",
+character:"Aino",
+name:"Reaction Support",
 
-    sets: [
-        {
-            name: "Marechaussee Hunter",
-            score: 100
-        },
-        {
-            name: "Heart of Depth",
-            score: 85
-        },
-        {
-            name: "Nymph's Dream",
-            score: 70
-        }
-    ],
+sets:[
+{name:"Silken Moon's Serenade",score:30},
+{name:"Noblesse Oblige",score:25},
+{name:"Scroll of the Hero od Cinder City",score:20},
+{name:"Deepwood Memories",score:15}
+],
 
-    mainStats: {
-        flower: [
-            "HP"
-        ],
+mainStats:{
+flower:["HP"],
+feather:["ATK"],
+sands:["Energy Recharge","Elemental Mastery"],
+goblet:["Elemental Mastery"],
+circlet:["Elemental Mastery","Crit Rate"]
+},
 
-        feather: [
-            "ATK"
-        ],
-
-        sands: [
-            "HP%"
-        ],
-
-        goblet: [
-            "Hydro DMG Bonus",
-            "HP%"
-        ],
-
-        circlet: [
-            "Crit Rate",
-            "Crit DMG",
-            "HP%"
-        ]
-    },
-
-    substats: {
-        "Crit Rate": 10,
-        "Crit DMG": 10,
-        "HP%": 10,
-        "Energy Recharge": 5,
-        "Elemental Mastery": 0,
-        "ATK%": 0,
-        "DEF%": 0,
-        "HP": 5,
-        "ATK": 0,
-        "DEF": 0
-    }
+substats:{
+"Crit Rate":3,
+"Crit DMG":0,
+"HP%":0,
+"ATK%":0,
+"DEF%":0,
+"Energy Recharge":10,
+"Elemental Mastery":8
 }
+},
 ];
 
 window.characters = characters;
